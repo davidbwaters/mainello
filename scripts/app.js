2bf895d782e77d7e4ca2d5b41685139f92086ff7
David B. Waters <mrdavidbwaters@gmail.com>
2021-04-05 21:08:03 -0400

    .

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,61 @@
 //
 
 console.log('♥️')
+
+import gsap from 'gsap'
+import scrollTrigger from 'gsap/ScrollTrigger'
+
+let rotateEl = document.querySelector('.c-contact-fab'),
+  rotateDuration = 10
+
+let rotate = gsap.to(rotateEl, {
+  rotation: 360,
+  duration: rotateDuration,
+  onReverseComplete() {
+    this.totalTime(rotateDuration * 100) // loop in reverse
+  },
+  repeat: -1,
+  ease: 'none'
+})
+
+let scrollTop,
+  contentHeight,
+  progress = 0,
+  rounds = 0,
+  clamp = gsap.utils.clamp(-50, 50)
+
+ScrollTrigger.create({
+  onUpdate: self => {
+    scrollTop =
+      window.pageYOffset ||
+      document.documentElement.scrollTop
+    contentHeight =
+      Math.max(
+        document.body.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight
+      ) - window.innerHeight
+    rounds = Math.floor(
+      contentHeight / window.innerHeight
+    )
+    progress =
+      Math.floor((scrollTop / contentHeight) * 100) *
+      rounds
+
+    console.log(clamp((self.getVelocity() / 100) * 0.5))
+    rotate.timeScale(clamp(self.getVelocity() / 100))
+    gsap.to(rotate, {
+      timeScale: self.direction,
+      duration: 0.3,
+      overwrite: true,
+      ease: 'power1.inOut'
+    })
+  }
+})
+
+gsap.set(rotateEl, {
+  transformOrigin: 'center center',
+  force3D: true
+})

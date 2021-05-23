deb666f4d1c04b1021813de2b5a9fd890059096e
David B. Waters <mrdavidbwaters@gmail.com>
2021-05-23 18:06:35 -0400

    .

--- .eleventy.js
+++ .eleventy.js
@@ -81,7 +81,6 @@ module.exports = function(eleventyConfig) {
         'scripts/app.js'
       ],
       outdir: 'build/scripts',
-      external: ['gsap'],
       bundle: true,
       minify: process.env.ELEVENTY_ENV === 'production',
       sourcemap: process.env.ELEVENTY_ENV !== 'production',
